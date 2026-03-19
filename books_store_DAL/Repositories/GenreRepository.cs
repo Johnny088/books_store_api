2ea3d105ef9252f0d22c8c9f585939226036502c
Johnny <esp25goodlife@gmail.com>
2026-03-19 05:35:04 +0200

    add genre repository/controller

--- books_store_DAL/Repositories/GenreRepository.cs
+++ books_store_DAL/Repositories/GenreRepository.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using books_store_DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace books_store_DAL.Repositories
 {
-    internal class GenreRepository
+    public class GenreRepository: GenericRepository<GenreEntity>
     {
+        private readonly AppDbContext _context;
+
+        public GenreRepository(AppDbContext appDbContext)
+            : base(appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public IQueryable<GenreEntity> Genres => GetAll();
+        public async Task<GenreEntity?> GetByNameAsync(string name)
+        {
+            return await Genres.FirstOrDefaultAsync(x => x.Name.ToLower() == name.ToLower());
+        }
     }
 }
---
+++ books_store_api/Controllers/GenreController.cs
@@ -0,0 +1,26 @@
+﻿using books_store_DAL;
+using books_store_DAL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace books_store_api.Controllers
+{
+    public class GenreController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly GenreRepository _genreRepository;
+
+        public GenreController(AppDbContext context, GenreRepository genreRepository)
+        {
+            _context = context;
+            _genreRepository = genreRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result =  await _genreRepository.Genres.ToListAsync();
+            return Ok(result);
+        }
+    }
+}
